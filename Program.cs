17e1b5a450de2535556b052711844946c4cbdcbe
Acer <Acer@ACER-PC45>
2021-02-17 10:55:15 +0200

    New tasks

--- Program.cs
+++ Program.cs
@@ -6,7 +6,47 @@ namespace Project
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+            Console.WriteLine("завдання 4.17 b");
+            double a, b;
+            Console.WriteLine("Площа кола =");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Площа трикутника =");
+            b = Convert.ToInt32(Console.ReadLine());
+            if (a >= b * Math.PI * 4 / (3 * Math.Sqrt(3))) 
+                Console.WriteLine("трикутник поміститься в колі");
+            else
+                Console.WriteLine("трикутник не поміститься в колі"); */
+
+            /*
+            Console.WriteLine("завдання 4.33");
+            double x;
+            Console.WriteLine("введіть число ");
+            x = Convert.ToInt32(Console.ReadLine());
+            if (x % 2 == 0 & x >= 0) 
+                Console.WriteLine("задане число є парним");
+            else
+                Console.WriteLine("задане число непарне"); */
+
+
+            /*
+            Console.WriteLine("завдання 4.49");
+            int a, b, c, d, x1, x2;
+            Console.WriteLine("input a=");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("input b=");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("input c=");
+            c = Convert.ToInt32(Console.ReadLine());
+            d = b * b - 4 * a * c;
+            x1 =Convert.ToInt32( -b + Math.Sqrt(d) / 2 * a);
+            x2 = Convert.ToInt32(-b - Math.Sqrt(d) / 2 * a);
+            if (d == 0 & a != 0)
+                Console.WriteLine($"Рівняння має 1 корінь, d= {d} x={x1}");
+            else if (d >= 0 & a != 0)
+                Console.WriteLine($"Рівняння має >1 кореня d= {d} x1={x1}, x2={x2}");
+            else
+                Console.WriteLine($"Рівняння не має коренів"); */
         }
     }
 }
